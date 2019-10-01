eca80d662339591461c9ec0b28b828ff3383911f
Sebastian Essink <sessink@users.noreply.github.com>
2019-10-01 14:56:01 -0700

    package deformtools

---
+++ src/deformtools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='deformtools',
+   version='1.0',
+   description='A module containing tools to analyse velocity gradients from drifters.',
+   author='Sebastian Essink',
+   author_email='sebastianessink@gmail.com',
+   packages=['deformtools'],  #same as name
+   # install_requires=['pandas', 'numpy'], #external packages as dependencies
+)
